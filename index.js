5be1725567bcb06239da51558d7b255ce522392b
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-16 21:49:06 +0500

    server created

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express')
 
 const app = express()
+
+app.use('/', (req, res) => {
+    console.log('this is home address');
+
+})
+
+app.listen(3001, (req, res) => {
+    console.log('server is created and working properly ');
+
+})
