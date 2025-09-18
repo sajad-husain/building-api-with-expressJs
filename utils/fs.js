96a93906e1d1ba6a4a6105ebff7cf03a5e7165ac
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-18 23:16:48 +0500

    the is a sync call

--- utils/fs.js
+++ utils/fs.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
 
 //sync mean it's a syncronous call
-fs.writeFileSync("./text.txt", "Salam Brother")
+// fs.writeFileSync("./text.txt", "Salam Brother")
+
+
+// Synchronous call
+fs.writeFile("./text.txt", "salam nabeel", err => { })
