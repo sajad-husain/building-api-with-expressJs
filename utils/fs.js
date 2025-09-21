032b98ef5b2b8906bb8b591e7e12ee97c9218575
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-21 23:54:42 +0500

    file restored

--- utils/fs.js
+++ utils/fs.js
@@ -1,30 +1,5 @@
 const fs = require('fs')
 
-//sync mean it's a syncronous call
-// fs.writeFileSync("./text.txt", "Salam Brother")
-
-
-// Asynchronous call
-// fs.writeFile("./text.txt", "salam nabeel", err => { })
-
-// Async call which reading this file utf-8 encoding, it encodes based on it's type
-// const readThisFile = fs.readFileSync("./text.txt", "utf-8")
-// console.log(readThisFile)f
-
-//sync call which reads data from file
-// main differece is in sync and async call is in sync call we need a callback function cz it does not return anything
-// fs.readFile("./text.txt", "utf-8", (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log("Reading file content =>", result);
-
-//     }
-
-//     console.log('file read successfully');
-
-// })
 
 // Real - World Scenario in Node.js Backend
 // A blocking HTTP server reads a file synchronously, causing other requests to wait until completion.
@@ -32,13 +7,4 @@ const fs = require('fs')
 // A non - blocking server reads files asynchronously, allowing other requests to proceed while waiting for the file read to complete.
 
 
-// append data in files operation behavious is same for sync and appenFile() method
-// fs.appendFileSync('./text.txt', ` Appending today's date which ic ${Date.now()}`)
-
-// delete file
-// fs.unlinkSync('./text.txt')
-
-// file status 
-// fs.statfsSync('./test.txt')
-console.log((fs.statSync('./test.txt')));
 
