64d57decce996893880f68c36b64d70138cfee52
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-21 09:45:28 +0500

    comment added

--- utils/fs.js
+++ utils/fs.js
@@ -7,8 +7,10 @@ const fs = require('fs')
 // Asynchronous call
 // fs.writeFile("./text.txt", "salam nabeel", err => { })
 
-// Async call which reading this file utf-8 encoding, it encodes based on it's type 
+// Async call which reading this file utf-8 encoding, it encodes based on it's type
 // const readThisFile = fs.readFileSync("./text.txt", "utf-8")
 // console.log(readThisFile)f
 
+//sync call which reads data from file
+// main differece is in sync and async call is in sync call we need a callback function cz it does not return anything
 fs.readFile("./text.txt", "utf-8", (err, result) => { })
