a1c1f0da47fb10b35e8d0df004e3ea94f81c3fb9
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-22 01:04:53 +0500

    a backend server which keeps the record of user's logs

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const http = require("http")
+const fs = require("fs")
 
 
 const server = http.createServer((req, res) => {
+    let counter = 1
+    const details = `${Date.now()} ${req.url} log created ${counter + 1} times\n`
+    fs.appendFile("./logdetails.txt", details, "utf-8", (err, result) => {
+        console.log(result);
+        switch (req.url) {
+            case '/': res.end("This is Hompepage")
+                break
+            case '/about': res.end("I'm sajjad hussain")
+                break
 
+        }
+    })
     res.end("server is created and running")
 })
 
