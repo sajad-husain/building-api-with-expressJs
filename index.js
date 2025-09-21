46c03e7648e5f8d57bceb2a208ae86c632697398
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-22 01:11:52 +0500

    app debugging completed

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const server = http.createServer((req, res) => {
     const details = `${Date.now()} ${req.url} log created ${counter + 1} times\n`
     fs.appendFile("./logdetails.txt", details, "utf-8", (err, result) => {
         console.log(result);
-        switch (req.url) {
-            case '/': res.end("This is Hompepage")
-                break
-            case '/about': res.end("I'm sajjad hussain")
-                break
-            default: res.end("EROOR 404 TRY ANOTHER ROUTE")
-
-        }
     })
-    res.end("server is created and running")
+    switch (req.url) {
+        case '/':
+            res.end("This is Hompepage")
+            break
+        case '/about':
+            res.end("I'm sajjad hussain")
+            break
+        default: res.end("EROOR 404 TRY ANOTHER ROUTE")
+
+    }
 })
 
 server.listen(3001, (req, res) => console.log('server is working properly'))
