a780b93a25832981a01f48d3a643137b021414b2
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-22 00:31:07 +0500

    os module used

--- utils/fs.js
+++ utils/fs.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const os = require('os')
 
 
 // Real - World Scenario in Node.js Backend
@@ -22,4 +23,8 @@ fs.readFile("./contact.txt", "utf-8", (err, result) => {
 
     }
 })
-console.log("after blocking");  
+console.log("after blocking");
+
+// checking cpu cors using os module
+console.log("You've ", os.cpus().length, "CPU cores");
+// node has thread equal to number of cpu cors by default it has 4 threads, threads are use to handle blocking operation / calls in node
