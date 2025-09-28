7686700d3ebf30734cc2a2814741c7a9aa247fcc
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-29 01:26:12 +0500

    file logs rearranged for installing express js

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const http = require("http")
+const fs = require("fs")
+const url = require("url")
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') return res.end("Homepage of cusotm website")
-})
+let counter = 0
+function handleFileLogs(req, res) {
+    counter += 1
+    const details = `${Date.now()}: requestd method is ${req.method} ${req.url} log created ${counter} times\n`
+    const myUrls = url.parse(req.url, true)
+    console.log(myUrls);
 
+    fs.appendFile("./logdetails.txt", details, "utf-8", (err, result) => {
+        console.log(result);
+    })
+
+    if (req.url === '/.well-known/appspecific/com.chrome.devtools.json') return res.end()
+    if (req.url === '/favicon.ico') return res.end()
+
+    switch (myUrls.pathname) {
+        case '/':
+            res.end("This is Hompepage")
+            break
+        case '/about':
+            const username = myUrls.query.myname
+            res.end("I'm " + username)
+            break
+        case '/search':
+            const search = myUrls.query.search
+            res.end("Your searched query is " + search)
+            break
+        default: res.end("EROOR 404 TRY ANOTHER ROUTE")
+
+    }
+}
+
+const server = http.createServer(handleFileLogs)
 server.listen(3001, (req, res) => console.log('server is working properly'))
