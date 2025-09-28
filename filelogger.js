3c66060aa84fbc65d8a142df8aa09bde571a7cf5
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-29 01:12:09 +0500

    code for file logs arranged properly

--- filelogger.js
+++ filelogger.js
@@ -2,28 +2,19 @@ const http = require("http")
 const fs = require("fs")
 const url = require("url")
 
-
+let counter = 0
 const server = http.createServer((req, res) => {
-    let counter = 1
-    const details = `${Date.now()} ${req.url} log created ${counter + 1} times\n`
+    counter += 1
+    const details = `${Date.now()} ${req.url} log created ${counter} times\n`
     const myUrls = url.parse(req.url, true)
     console.log(myUrls);
 
-
-
     fs.appendFile("./logdetails.txt", details, "utf-8", (err, result) => {
         console.log(result);
-        console.log(err);
-
     })
 
-    if (req.url === '/.well-known/appspecific/com.chrome.devtools.json') {
-        res.end()
-    }
-
-    if (req.url === '/favicon.ico') {
-        res.end()
-    }
+    if (req.url === '/.well-known/appspecific/com.chrome.devtools.json') return res.end()
+    if (req.url === '/favicon.ico') return res.end()
 
     switch (myUrls.pathname) {
         case '/':
@@ -34,8 +25,8 @@ const server = http.createServer((req, res) => {
             res.end("I'm " + username)
             break
         case '/search':
-            const serach = myUrls.query.search
-            res.end("Your searched query is" + search)
+            const search = myUrls.query.search
+            res.end("Your searched query is " + search)
             break
         default: res.end("EROOR 404 TRY ANOTHER ROUTE")
 
--- index.js
+++ index.js
@@ -1,36 +1,7 @@
 const http = require("http")
-const fs = require("fs")
-const url = require("url")
 
-let counter = 0
 const server = http.createServer((req, res) => {
-    counter += 1
-    const details = `${Date.now()} ${req.url} log created ${counter} times\n`
-    const myUrls = url.parse(req.url, true)
-    console.log(myUrls);
-
-    fs.appendFile("./logdetails.txt", details, "utf-8", (err, result) => {
-        console.log(result);
-    })
-
-    if (req.url === '/.well-known/appspecific/com.chrome.devtools.json') return res.end()
-    if (req.url === '/favicon.ico') return res.end()
-
-    switch (myUrls.pathname) {
-        case '/':
-            res.end("This is Hompepage")
-            break
-        case '/about':
-            const username = myUrls.query.myname
-            res.end("I'm " + username)
-            break
-        case '/search':
-            const search = myUrls.query.search
-            res.end("Your searched query is" + search)
-            break
-        default: res.end("EROOR 404 TRY ANOTHER ROUTE")
-
-    }
+    if (req.url === '/') return res.end("Homepage of cusotm website")
 })
 
 server.listen(3001, (req, res) => console.log('server is working properly'))
