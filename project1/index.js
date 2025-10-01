11f3bfff578358ca7bbbfa0d5f407fcf9bad1d3c
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-01 21:30:38 +0500

    getting a json response on /users rout

--- project1/index.js
+++ project1/index.js
@@ -1,10 +1,15 @@
 const express = require("express")
+const users = require("./mockusers.json")
 
 const port = 3005
 const app = express()
 
 app.get('/', (req, res) => {
     res.send("this is homepage")
+})
+
+app.get('/users', (req, res) => {
+    return res.json(users)
 })
 
 app.listen(port, (req, res) => {
