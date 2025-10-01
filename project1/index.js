fd4c14396ae2f0924d48908662c59d4136a2a3b3
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-01 21:15:49 +0500

    basic and base route woring on my app

--- project1/index.js
+++ project1/index.js
@@ -3,6 +3,10 @@ const express = require("express")
 const port = 3005
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("this is homepage")
+})
+
 app.listen(port, (req, res) => {
     console.log("Server is working");
 
