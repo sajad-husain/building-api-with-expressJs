3a84ac33d09a34dcd8bf91b650389925dbf516e2
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-03 23:02:31 +0500

    user route defined in my fule

--- project1/index.js
+++ project1/index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.send("this is homepage")
 })
 
+app.get('/users', (req, res) => {
+    const html = `<ul>
+    ${users.map(item => `<li>${item.firstName}</li>`).join('')}
+        </ul>
+    `;
+    res.send(html)
+})
+
 app.get('/api/users', (req, res) => {
     return res.json(users)
 })
