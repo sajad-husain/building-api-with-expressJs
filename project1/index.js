45fd3cf57e005c73d121df255b834879e0014c0a
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-06 00:08:24 +0500

    route for getting a user is working

--- project1/index.js
+++ project1/index.js
@@ -19,7 +19,7 @@ app.get('/users', (req, res) => {
 
 app.get("/api/users/:id", (req, res) => {
     const id = req.params.id
-    const user = users.filter((item) => item.id === id)
+    const user = users.find((item) => item.id === id)
     if (!user) {
         return res.status(404).json({ error: "User not found" })
     }
