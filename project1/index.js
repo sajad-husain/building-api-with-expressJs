92f6819a62a4ed44fbde76e979a7b048104ef4a1
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-19 01:06:15 +0500

    mongoose db conntection established

--- project1/index.js
+++ project1/index.js
@@ -8,6 +8,9 @@ const { type } = require("os")
 const port = 3005
 const app = express()
 
+//DB CONNECTION
+mongoose.connect('mongodb://127.0.0.1:27017')
+
 //Mongoose schema
 const userSchema = new mongoose.Schema({
     firstName: {
