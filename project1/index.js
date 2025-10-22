1bbd717ca6205ec14d11fc16c87db6e43d464864
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-22 23:07:24 +0500

    essential changes made

--- project1/index.js
+++ project1/index.js
@@ -58,7 +58,7 @@ app.get("/api/users/:id", (req, res) => {
     const id = Number(req.params.id)
     const user = users.find((item) => item.id === id)
     if (!user) {
-        return res.status(404).json({ error: "User not found" })
+        return res.status(404).json({ error: `User not found on id:${id}` })
     }
     return res.json(user)
 })
@@ -66,6 +66,8 @@ app.get("/api/users/:id", (req, res) => {
 // post request
 app.post("/api/users", (req, res) => {
     const body = req.body
+    console.log(body);
+
     users.push({ ...body, id: users.length + 1 })
     fs.writeFile("./mockusers.json", JSON.stringify(users), (err, data) => {
         if (err) return res.status(505).json({ err: "failed to make post request" })
